d2f7c2331422c7ab66aa6c7bb3cae90c1169c054
Kaviselvam31 <kaviselvam505@gmail.com>
2026-07-24 23:36:06 +0530

    Day 2: Added Branch POST API and service layer

--- src/main/java/com/storeflow/controller/BranchController.java
+++ src/main/java/com/storeflow/controller/BranchController.java
@@ -1,8 +1,13 @@
 package com.storeflow.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.storeflow.entity.Branch;
+import com.storeflow.service.BranchService;
 
 @RestController
 @RequestMapping("/branch")
@@ -11,6 +16,14 @@ public class BranchController {
     @GetMapping("/hello")
     public String hello() {
         return "Welcome to StoreFlow";
-    }
+    }private final BranchService branchService;
+
+public BranchController(BranchService branchService) {
+    this.branchService = branchService;
+}
+@PostMapping
+public Branch saveBranch(@RequestBody Branch branch) {
+    return branchService.saveBranch(branch);
+}
 
 }
--- src/main/java/com/storeflow/service/BranchService.java
+++ src/main/java/com/storeflow/service/BranchService.java
@@ -1,14 +1,19 @@
 package com.storeflow.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.storeflow.entity.Branch;
 import com.storeflow.repository.BranchRepository;
 
 @Service
 public class BranchService {
 
-    @Autowired
-    private BranchRepository branchRepository;
+    private final BranchRepository branchRepository;
 
+    public BranchService(BranchRepository branchRepository) {
+        this.branchRepository = branchRepository;
+    }
+public Branch saveBranch(Branch branch) {
+    return branchRepository.save(branch);
+}
 }
