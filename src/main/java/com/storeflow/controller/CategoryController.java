29a4fc657dde0fd72f46edb8b7098d75fe577d4e
Kaviselvam31 <kaviselvam505@gmail.com>
2026-08-01 21:40:05 +0530

    Day 7: Completed Category CRUD API's

--- src/main/java/com/storeflow/controller/CategoryController.java
+++ src/main/java/com/storeflow/controller/CategoryController.java
@@ -1,5 +1,7 @@
 package com.storeflow.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.*;
 
 import com.storeflow.entity.Category;
@@ -19,5 +21,30 @@ public class CategoryController {
     @PostMapping
     public Category saveCategory(@RequestBody Category category) {
         return categoryService.saveCategory(category);
+    }
+
+    // GET ALL API
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    // GET BY ID API
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable int id) {
+        return categoryService.getCategoryById(id);
+    }
+
+    // UPDATE API
+    @PutMapping("/{id}")
+    public Category updateCategory(@PathVariable int id,
+                                   @RequestBody Category category) {
+        return categoryService.updateCategory(id, category);
+    }
+
+    // DELETE API
+    @DeleteMapping("/{id}")
+    public String deleteCategory(@PathVariable int id) {
+        return categoryService.deleteCategory(id);
     }
 }
--- src/main/java/com/storeflow/service/CategoryService.java
+++ src/main/java/com/storeflow/service/CategoryService.java
@@ -1,5 +1,7 @@
 package com.storeflow.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.storeflow.entity.Category;
@@ -17,5 +19,41 @@ public class CategoryService {
     // POST API
     public Category saveCategory(Category category) {
         return categoryRepository.save(category);
+    }
+
+    // GET ALL API
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    // GET BY ID API
+    public Category getCategoryById(int id) {
+        return categoryRepository.findById(id).orElse(null);
+    }
+
+    // UPDATE API
+    public Category updateCategory(int id, Category category) {
+
+        Category existingCategory = categoryRepository.findById(id).orElse(null);
+
+        if (existingCategory != null) {
+            existingCategory.setCategoryName(category.getCategoryName());
+            existingCategory.setDescription(category.getDescription());
+
+            return categoryRepository.save(existingCategory);
+        }
+
+        return null;
+    }
+
+    // DELETE API
+    public String deleteCategory(int id) {
+
+        if (categoryRepository.existsById(id)) {
+            categoryRepository.deleteById(id);
+            return "Category Deleted Successfully";
+        }
+
+        return "Category Not Found";
     }
 }
